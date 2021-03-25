2c68b934b05cfa8c72b2f390be8af9414ac1d28e
AustinJoo97 <austinjoo97@gmail.com>
2021-03-25 19:56:53 -0400

    added additional id's to html tags; created retrieveMovies function which successfully takes the options from the dropdown and the query; need to implement functionality to successfully retrieve movies from API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 let apiKey = 'f11af3c11ab7492603919de407bd7900';
 let movieImgURL = 'https://image.tmdb.org/t/p/original/';
+let searchQueries = '';
 // User poster path as endpoint for the above URL
 let genresDropdown = document.getElementById('genresDropdown');
+let searchForMovies = document.getElementById('searchOptions')
 
 
 function initializer(){
@@ -24,5 +26,34 @@ function categoriesPopulation(){
     })
 }
 
+function retrieveMovies(event){
+    event.preventDefault();
+    let searchCriteriaChosen = this.childNodes[1].value;
+    let searchQuery = this.childNodes[3].value;
+
+    if(!searchQuery){
+        return;
+    }
+
+    // This returns for the selected value of the searchOptions' dropdown menu
+    console.log(this.childNodes[1].value);
+    // console.log(typeof searchCriteriaChosen);
+    if(searchCriteriaChosen === 'none'){
+
+    } else if(searchCriteriaChosen === 'actor'){
+
+    } else if(searchCriteriaChosen === 'yearReleased'){
+
+    } else if(searchCriteriaChosen === 'title'){
+
+    } else if(searchCriteriaChosen === 'rating'){
+
+    }
+
+    // This returns the value within the input box
+    console.log(this.childNodes[3].value)
+}
+
 
 initializer();
+searchForMovies.addEventListener("submit", retrieveMovies)
