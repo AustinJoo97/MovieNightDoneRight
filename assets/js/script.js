4b8d6332d2a18d0352f672cdb8587cd8a0bc0c11
AustinJoo97 <austinjoo97@gmail.com>
2021-03-27 12:01:35 -0400

    completely and successfully makes API calls based on movie name, keywords, year released, and cast member names to retrieve a list of movies with all relevant details to be rendered to DOM; next step is to retrieve movies by parental ratings

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,11 +28,12 @@ function categoriesPopulation(){
 
 function retrieveMovies(event){
     event.preventDefault();
-    let searchCriteriaChosen = this.childNodes[1].value;
-    let searchQuery = this.childNodes[3].value;
+    let searchCriteriaChosen = this.childNodes[1].childNodes[1].value;
+    let searchQuery = this.childNodes[1].childNodes[3].value;
     let searchQueryArray;
     let urlQuery = '';
 
+    
     if(!searchQuery){
         urlQuery= 'all';
         getMoviesByTitle(urlQuery);
@@ -51,9 +52,16 @@ function retrieveMovies(event){
         getMoviesByTitle(urlQuery);
         return;
     } else if(searchCriteriaChosen === 'actor'){
-
-
-
+        searchQueryArray = searchQuery.split(" ");
+        for(let i = 0; i < searchQueryArray.length; i++){
+            if(i === searchQueryArray.length-1){
+                urlQuery += `${searchQueryArray[i]}`
+            } else {
+                urlQuery += `${searchQueryArray[i]}%20`
+            }
+        }
+        getMoviesByActors(urlQuery);
+        return;
     } else if(searchCriteriaChosen === 'yearReleased'){
         if(Number(searchQuery)){
             urlQuery = `${searchQuery}`;
@@ -63,33 +71,67 @@ function retrieveMovies(event){
         }
         return;
     } else if(searchCriteriaChosen === 'rating'){
-
         
     }
 }
 
-function getMoviesByTitle(queryString){
-    // console.log(`THIS IS QUERY STRING: ${queryString}`)
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&query=${queryString}&language=en-US&include_adult=true`)
+
+// These functions pertain to searches made regarding a movie's information (title, year released)
+function getMoviesByTitle(movieTitleString){
+    // console.log(`THIS IS QUERY STRING: ${movieTitleString}`)
+    fetch(`${tmdbAPI}${apiKey}&language=en-US&sort_by=popularity.desc&query=${movieTitleString}&include_adult=true`)
     // Sort_by=popularity.desc is not working. Look into this later
     .then(function(response){
         return response.json()
     })
     .then(function(data){
-        console.log(data);
+        data.results.forEach(getFullMovieDetails)
     })
 }
 
-function getMoviesByYear(queryNumAsString){
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&query=all&language=en-US&include_adult=true&primary_release_year=${queryNumAsString}`)
-    // Sort_by=popularity.desc is not working. Look into this later
+function getMoviesByYear(movieReleaseYearString){
+    fetch(`${tmdbAPI}${apiKey}&language=en-US&sort_by=popularity.desc&query=allS&include_adult=true&primary_release_year=${movieReleaseYearString}`)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        data.results.forEach(getFullMovieDetails)
+    })
+}
+
+
+// These functions pertain to searches made regarding a movie's cast and his/her name
+function getMoviesByActors(actorNameString){
+    fetch(`http://api.tmdb.org/3/search/person?api_key=${apiKey}&query=${actorNameString}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        getActorsFilmography(data.results[0].id)
+    })
+}
+    // This function will take the actor's ID passed from the above function and return his/her full filmography
+    // It will then take the ID of each film and pass it into the getFullMovieDetailsFunction
+function getActorsFilmography(actorID){
+    fetch(`https://api.themoviedb.org/3/person/${actorID}/movie_credits?api_key=${apiKey}`)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        data.cast.forEach(getFullMovieDetails)
+    })
+}
+
+// This function will get the full details of all the movies that are returned in the above functions
+function getFullMovieDetails(movie){
+    fetch(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${apiKey}&append_to_response=credits`)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
         console.log(data);
     })
-}
+};
 
 initializer();
 searchForMovies.addEventListener("submit", retrieveMovies)
