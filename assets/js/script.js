554a9f2b5222465a33ccdc372520b23c5c1cddc7
Brandon Ford <Brandon.ford.617@gmail.com>
2021-03-30 19:09:26 -0400

    added placeholder for if img throws error

--- assets/js/script.js
+++ assets/js/script.js
@@ -198,6 +198,8 @@ function getFullMovieDetails(movie){
         newMovieCell.appendChild(newMovieCard);
         newMovieCell.setAttribute('class','column');
         renderedMovies.appendChild(newMovieCell);
+        //if img throws an error the src will change to the new placeholder 
+        newMovieImg.setAttribute('onerror',"this.onerror=null;this.src='https://placehold.it/300x400'")
     })
 };
 
